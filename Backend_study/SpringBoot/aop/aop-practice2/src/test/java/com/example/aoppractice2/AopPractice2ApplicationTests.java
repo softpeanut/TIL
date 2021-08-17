c70b16b4422e3887585219a1f4b253d4fd9a6b39
김범진 <sdpthf@naver.com>
2021-08-17 20:08:54 +0900

    Tests

---
+++ Backend_study/SpringBoot/aop/aop-practice2/src/test/java/com/example/aoppractice2/AopPractice2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.aoppractice2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AopPractice2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
