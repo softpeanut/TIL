7d5cf3a8ec25dd218fa7f09306f4e148db95d28b
김범진 <sdpthf@naver.com>
2021-08-18 20:09:51 +0900

    [UPDATE] AspectProvider

--- Backend_study/SpringBoot/aop/aop-practice2/src/main/java/com/example/aoppractice2/annotation/AspectProvider.java
+++ Backend_study/SpringBoot/aop/aop-practice2/src/main/java/com/example/aoppractice2/annotation/AspectProvider.java
@@ -2,8 +2,10 @@ package com.example.aoppractice2.annotation;
 
 import lombok.extern.slf4j.Slf4j;
 import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.After;
 import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
 import org.springframework.stereotype.Component;
 
 @Aspect
@@ -14,6 +16,7 @@ public class AspectProvider {
     @Around("@annotation(com.example.aoppractice2.annotation.PerformanceTimeRecord)")
     public Object logExecutionTime(ProceedingJoinPoint joinPoint) throws Throwable {
         long start = System.currentTimeMillis();
+        log.info("First Log");
 
         Object proceed = joinPoint.proceed();
 
@@ -21,6 +24,17 @@ public class AspectProvider {
 
         log.info(joinPoint.getSignature() + " executed in " + executionTime + "ms");
         return proceed;
+    }
+
+
+    @Before("@annotation(com.example.aoppractice2.annotation.PersonInfo))")
+    public void logBeforePersonInfo() {
+        log.info("Before Log");
+    }
+
+    @After("@annotation(com.example.aoppractice2.annotation.PersonInfo))")
+    public void logAfterPersonInfo() {
+        log.info("After Log");
     }
 
 }
