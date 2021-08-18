e5a62af8469cb4461b5a72e79379c4bf041a3262
김범진 <sdpthf@naver.com>
2021-08-18 17:09:42 +0900

    [ADD] PerformanceTimeRecord

---
+++ Backend_study/SpringBoot/aop/aop-practice2/src/main/java/com/example/aoppractice2/annotation/PerformanceTimeRecord.java
@@ -0,0 +1,11 @@
+package com.example.aoppractice2.annotation;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Target(ElementType.METHOD)
+@Retention(RetentionPolicy.RUNTIME)
+public @interface PerformanceTimeRecord {
+}
