5dac5893b63971733bd7fcb508d4f043c545f9c3
김범진 <sdpthf@naver.com>
2021-09-03 19:33:42 +0900

    [ADD] Request

---
+++ Backend_study/SpringBoot/image/image-upload/src/main/java/com/example/image/payload/Request.java
@@ -0,0 +1,11 @@
+package com.example.image.payload;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@AllArgsConstructor @NoArgsConstructor
+public class Request {
+    private String fileName;
+}
