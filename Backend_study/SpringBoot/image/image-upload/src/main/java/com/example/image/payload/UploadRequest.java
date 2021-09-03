731f8fa6cdabb3c106c3d9cf87743f07c8997c9b
김범진 <sdpthf@naver.com>
2021-09-03 19:34:32 +0900

    [ADD] UploadRequest

---
+++ Backend_study/SpringBoot/image/image-upload/src/main/java/com/example/image/payload/UploadRequest.java
@@ -0,0 +1,15 @@
+package com.example.image.payload;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import org.springframework.web.multipart.MultipartFile;
+import java.util.List;
+
+@Getter
+@AllArgsConstructor
+public class UploadRequest {
+    private String name;
+    private int age;
+
+    private List<MultipartFile> multipartFiles;
+}
