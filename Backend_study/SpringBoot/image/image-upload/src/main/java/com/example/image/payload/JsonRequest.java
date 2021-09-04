f8c866caa8d068de78f4d6e4e2b167ccddc0f760
김범진 <sdpthf@naver.com>
2021-09-04 18:29:33 +0900

    [ADD] JsonRequest

---
+++ Backend_study/SpringBoot/image/image-upload/src/main/java/com/example/image/payload/JsonRequest.java
@@ -0,0 +1,11 @@
+package com.example.image.payload;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class JsonRequest {
+    private String name;
+    private int age;
+}
